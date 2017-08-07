6907f6c77400b465f1ed5bfb02317035c78525de
dpalmerGitHub <dpalmer212@hotmail.com>
2017-08-07 10:26:55 -0400

    Full Stack MEAN application
    MEAN application design for blog posts

---
+++ mean-blog/server.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blogposts2017');
+var PostSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    body: String,
+    tag: { type: String, enum: ['POLITICS,ECONOMY,SCIENCE,CODING'] },
+    posted: { type: Date, default: Date.now }
+
+}, { collection: "blogpost" });
+
+var PostModel = mongoose.model("PostModel", PostSchema);
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/api/blogpost", createPost);
+app.get("/api/blogpost", getAllPosts);
+app.get("/api/blogpost/:id", getPostById);
+app.put("/api/blogpost/:id", updatePost);
+app.delete("/api/blogpost/:id", deletePost);
+
+function updatePost(req, res) {
+    var postId = req.params.id;
+    var post = req.body;
+    PostModel.update({
+        _id: postId
+    }, {
+        title: post.title,
+        body: post.body
+    }).then(
+        function(status) {
+            res.json(status);
+        },
+        function(error) {
+            res.sendStatus(400);
+        }
+    )
+}
+
+function getPostById(req, res) {
+    var postId = req.params.id;
+    PostModel.findById({
+        _id: postId
+    }).then(
+        function(post) {
+            res.json(post);
+        },
+        function(error) {
+            res.sendStatus(400);
+        }
+    )
+}
+
+
+function deletePost(req, res) {
+    var postId = req.params.id;
+    PostModel.remove({
+        _id: postId
+    }).then(
+        function(status) {
+            res.sendStatus(200);
+        },
+        function(error) {
+            res.sendStatus(400);
+        }
+    )
+}
+
+function getAllPosts(req, res) {
+    PostModel
+        .find()
+        .then(
+            function(posts) {
+                res.json(posts);
+            },
+            function(error) {
+                res.sendStatus(400);
+            }
+        )
+}
+
+function createPost(req, res) {
+    var post = req.body;
+    PostModel.create(post)
+        .then(
+            function(postObj) {
+                res.json(200);
+            },
+            function(error) {
+                res.sendStatus(400);
+            }
+        );
+}
+
+
+app.listen(3000);
